98887bb372ca743c63a8bcdd698add81f58de79b
lucasreali <lucasrealioficial@gmail.com>
2026-01-06 11:03:29 -0300

    feat: initialize router with authentication context and toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,40 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner';
 import './index.css';
+import { checkAuth } from './lib/check-auth';
+import { routeTree } from './routeTree.gen';
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <div>teste</div>
-    </StrictMode>
-);
+const router = createRouter({
+    routeTree,
+    context: {
+        auth: undefined!,
+    },
+});
+
+declare module '@tanstack/react-router' {
+    interface Register {
+        router: typeof router;
+    }
+}
+
+async function initApp() {
+    const auth = await checkAuth();
+
+    const domElementId = 'root';
+
+    const rootElement = document.getElementById(domElementId);
+    if (!rootElement) {
+        throw new Error(`Element with id ${domElementId} not found`);
+    }
+
+    createRoot(rootElement).render(
+        <StrictMode>
+            <RouterProvider router={router} context={{ auth }} />
+            <Toaster />
+        </StrictMode>
+    );
+}
+
+initApp();
