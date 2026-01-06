85328909a37dd2083541c75004708749783239f8
lucasreali <lucasrealioficial@gmail.com>
2026-01-06 11:03:29 -0300

    build(vite): add TanStack Router and Tailwind CSS plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import tailwindcss from '@tailwindcss/vite';
-import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        tanstackRouter({
-            target: 'react',
-            autoCodeSplitting: true,
-        }),
-        tailwindcss(),
-        react(),
-    ],
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
+		tailwindcss(),
+		react(),
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
